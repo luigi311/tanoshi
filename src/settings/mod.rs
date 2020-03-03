6ea535f2667fbd848bc20df9e01bae4326c2c8b2
faldez <github@faldez.dev>
2020-03-03 08:46:32 +0700

    rename direction to reading_direction and use enum for direction and background

--- src/settings/mod.rs
+++ src/settings/mod.rs
@@ -2,10 +2,23 @@ use serde::{Deserialize, Serialize};
 pub mod settings;
 
 #[derive(Deserialize, Serialize, Clone)]
+pub enum ReadingDirection {
+    LeftToRight,
+    RightToLeft,
+    LongStrip,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub enum BackgroundColor {
+    Black,
+    White,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
 pub struct SettingParams {
     pub double_page: bool,
-    pub background_color: bool,
-    pub direction: i32,
+    pub background_color: BackgroundColor,
+    pub reading_direction: ReadingDirection,
 }
 
 #[derive(Deserialize, Serialize, Clone)]
