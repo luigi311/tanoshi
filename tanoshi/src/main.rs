51b98d67e5348baeedee6d1fa8e342b4f1aa8bad
faldez <github@faldez.dev>
2021-05-30 22:44:00 +0700

    v0.22.2

--- tanoshi/src/main.rs
+++ tanoshi/src/main.rs
@@ -32,7 +32,7 @@ use warp::http::{Response as HttpResponse, StatusCode};
 use warp::{Filter, Rejection};
 
 #[derive(Clap)]
-#[clap(version = "0.22.1")]
+#[clap(version = "0.22.2")]
 struct Opts {
     /// Path to config file
     #[clap(long)]
