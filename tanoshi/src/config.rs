d3a2c96c5e28b993938eff77877188415044dbd8
faldez <github@faldez.dev>
2021-06-27 23:25:20 +0700

    use tanoshi_home to specify tanoshi parent directory

--- tanoshi/src/config.rs
+++ tanoshi/src/config.rs
@@ -7,26 +7,35 @@ use std::{iter, path::PathBuf};
 pub struct Config {
     #[serde(default = "default_port")]
     pub port: u16,
+    #[serde(default = "default_database_path")]
     pub database_path: String,
     #[serde(default = "default_secret")]
     pub secret: String,
     #[serde(default = "default_update_interval")]
     pub update_interval: u64,
+    #[serde(default = "default_plugin_path")]
     pub plugin_path: String,
-    #[serde(default)]
+    #[serde(default = "default_local_path")]
     pub local_path: String,
 }
 
-impl Config {
-    fn new(database_path: String, plugin_path: String, local_path: String) -> Self {
+impl Default for Config {
+    fn default() -> Self {
         Self {
             port: default_port(),
-            database_path,
+            database_path: default_database_path(),
             secret: default_secret(),
             update_interval: default_update_interval(),
-            plugin_path,
-            local_path,
+            plugin_path: default_plugin_path(),
+            local_path: default_local_path(),
         }
+    }
+}
+
+fn tanoshi_home() -> PathBuf {
+    match std::env::var("TANOSHI_HOME") {
+        Ok(path) => PathBuf::from(path),
+        Err(_) => dirs::home_dir().expect("should have home").join(".tanoshi"),
     }
 }
 
@@ -47,36 +56,45 @@ fn default_secret() -> String {
     String::from_utf8(chars).unwrap()
 }
 
+fn default_database_path() -> String {
+    let path = tanoshi_home();
+    if !path.exists() {
+        let _ = std::fs::create_dir_all(&path);
+    }
+    path.join("tanoshi.db").to_str().unwrap().to_string()
+}
+
+fn default_plugin_path() -> String {
+    let path = tanoshi_home().join("plugins");
+    if !path.exists() {
+        let _ = std::fs::create_dir_all(&path);
+    }
+    path.to_str().unwrap().to_string()
+}
+
+fn default_local_path() -> String {
+    let path = tanoshi_home().join("manga");
+    if !path.exists() {
+        let _ = std::fs::create_dir_all(&path);
+    }
+    path.to_str().unwrap().to_string()
+}
+
 impl Config {
     pub fn open(path: Option<String>) -> Result<Config, Box<dyn std::error::Error>> {
         let config_path = match path {
             Some(p) => PathBuf::from(p.clone()),
-            None => dirs::home_dir()
-                .expect("should be home dir, or define path")
-                .join(".tanoshi")
-                .join("config.yml"),
+            None => tanoshi_home().join("config.yml"),
         };
 
         match std::fs::File::open(config_path.clone()) {
             Ok(file) => {
                 info!("Open config from {:?}", config_path);
-                serde_yaml::from_reader(file)
-                    .map_err(|err| format!("error parse yaml: {}", err).into())
+                Ok(serde_yaml::from_reader(file)?)
             }
             Err(_) => {
-                let parent = config_path.parent().unwrap();
-                let database_path = parent
-                    .join("tanoshi")
-                    .with_extension("db")
-                    .to_str()
-                    .unwrap()
-                    .to_string();
-                let plugin_path = parent.join("plugins").to_str().unwrap().to_string();
-                let local_path = parent.join("manga").to_str().unwrap().to_string();
-                let config = Config::new(database_path, plugin_path, local_path);
-
-                let _ = std::fs::create_dir_all(config_path.parent().expect("should have parent"));
-                let _ = std::fs::write(&config_path, serde_yaml::to_string(&config).unwrap());
+                let config = Config::default();
+                std::fs::write(&config_path, serde_yaml::to_string(&config)?)?;
                 info!("Write default config at {:?}", config_path);
                 Ok(config)
             }
