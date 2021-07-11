be825f4e9666ceb7b6577129cc23d344fc767fb4
faldez <github@faldez.dev>
2021-07-11 08:11:22 +0700

    remove any unwrap from local to avoid panic

--- tanoshi/src/local.rs
+++ tanoshi/src/local.rs
@@ -16,6 +16,10 @@ impl Local {
         Box::new(Self { path })
     }
 
+    fn default_cover_url() -> String {
+        "/images/cover-placeholder.jpg".to_string()
+    }
+
     fn filter_file_only(entry: Result<DirEntry, std::io::Error>) -> Option<DirEntry> {
         match entry {
             Ok(res) => {
@@ -26,6 +30,34 @@ impl Local {
                 }
             }
             Err(_) => None,
+        }
+    }
+
+    fn find_cover_url(entry: &PathBuf) -> String {
+        let entry_read_dir = match entry.read_dir() {
+            Ok(entry_read_dir) => entry_read_dir,
+            Err(e) => {
+                return Self::default_cover_url();
+            }
+        };
+
+        let dir_entry = match entry_read_dir
+            .into_iter()
+            .filter_map(Self::filter_file_only)
+            .next()
+        {
+            Some(entry) => entry,
+            None => {
+                return Self::default_cover_url();
+            }
+        };
+        let path = dir_entry.path();
+        match libarchive_rs::list_archive_files(format!("{}", path.display()).as_str()) {
+            Ok(files) => files
+                .first()
+                .map(|first_page| format!("{}", path.join(first_page).display()))
+                .unwrap_or_else(Self::default_cover_url),
+            Err(_) => Self::default_cover_url(),
         }
     }
 }
@@ -52,37 +84,16 @@ impl Extension for Local {
 
         let mut data = vec![];
         for entry in read_dir.into_iter().filter_map(|entry| entry.ok()) {
-            if let Ok(entry_read_dir) = entry.path().read_dir() {
-                let mut cover_url = "".to_string();
-                if let Some(entry) = entry_read_dir
-                    .into_iter()
-                    .filter_map(Self::filter_file_only)
-                    .next()
-                {
-                    let path = entry.path();
-                    match libarchive_rs::list_archive_files(path.to_str().unwrap()) {
-                        Ok(files) => {
-                            info!("open {:?}", path);
-                            let first_page = files.first().unwrap().clone();
-                            cover_url = path.join(first_page).to_str().unwrap().to_string();
-                        }
-                        Err(e) => {
-                            return ExtensionResult::err(format!("{}", e).as_str());
-                        }
-                    }
-                }
-
-                data.push(Manga {
-                    source_id: ID,
-                    title: entry.file_name().to_str().unwrap().to_string(),
-                    author: vec![],
-                    genre: vec![],
-                    status: None,
-                    description: None,
-                    path: entry.path().to_str().unwrap().to_string(),
-                    cover_url,
-                })
-            }
+            data.push(Manga {
+                source_id: ID,
+                title: entry.file_name().to_str().unwrap_or("").to_string(),
+                author: vec![],
+                genre: vec![],
+                status: None,
+                description: None,
+                path: entry.path().to_str().unwrap_or("").to_string(),
+                cover_url: Self::find_cover_url(&entry.path()),
+            })
         }
 
         ExtensionResult::ok(data)
@@ -91,27 +102,13 @@ impl Extension for Local {
     fn get_manga_info(&self, path: String) -> ExtensionResult<Manga> {
         let path = PathBuf::from(&path);
 
-        let mut cover_url = "".to_string();
-        if let Ok(entry_read_dir) = path.read_dir() {
-            if let Some(entry) = entry_read_dir
-                .into_iter()
-                .filter_map(Self::filter_file_only)
-                .next()
-            {
-                let path = entry.path();
-                match libarchive_rs::list_archive_files(path.to_str().unwrap()) {
-                    Ok(files) => {
-                        let first_page = files.first().unwrap().clone();
-                        cover_url = path.join(first_page).to_str().unwrap().to_string();
-                    }
-                    Err(e) => {
-                        return ExtensionResult::err(format!("{}", e).as_str());
-                    }
-                }
-            }
-        }
+        let cover_url = Self::find_cover_url(&path);
 
-        let title = path.file_name().unwrap().to_str().unwrap().to_string();
+        let title = path
+            .file_name()
+            .and_then(|filename| filename.to_str())
+            .unwrap_or("")
+            .to_string();
         ExtensionResult::ok(Manga {
             source_id: ID,
             title: title.clone(),
@@ -119,7 +116,7 @@ impl Extension for Local {
             genre: vec![],
             status: Some("".to_string()),
             description: Some(title),
-            path: path.to_str().unwrap().to_string(),
+            path: path.to_str().unwrap_or("").to_string(),
             cover_url,
         })
     }
@@ -134,34 +131,44 @@ impl Extension for Local {
 
         let mut data = vec![];
         for entry in read_dir.into_iter().filter_map(|entry| entry.ok()) {
-            let modified = entry
+            let modified = match entry
                 .metadata()
-                .unwrap()
-                .modified()
-                .unwrap()
-                .duration_since(UNIX_EPOCH)
-                .unwrap()
-                .as_secs();
-            let number_re =
-                Regex::new(r"(?i)(?<=v)(\d+)|(?<=volume)\s*(\d+)|(?<=vol)\s*(\d+)|(?<=\s)(\d+)")
-                    .unwrap();
-            let file_name = entry
+                .ok()
+                .and_then(|metadata| metadata.modified().ok())
+                .and_then(|modified| modified.duration_since(UNIX_EPOCH).ok())
+            {
+                Some(modified) => modified.as_secs(),
+                None => {
+                    return ExtensionResult::err("failed to get modified date");
+                }
+            };
+            let number_re = match Regex::new(
+                r"(?i)(?<=v)(\d+)|(?<=volume)\s*(\d+)|(?<=vol)\s*(\d+)|(?<=\s)(\d+)",
+            ) {
+                Ok(re) => re,
+                Err(e) => {
+                    return ExtensionResult::err(format!("failed to create regex: {}", e).as_str());
+                }
+            };
+            let file_name = match entry
                 .path()
                 .file_stem()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string();
-            info!("local: {}", file_name);
-            let mat = number_re.find(&file_name).unwrap();
-            let number = mat
-                .map(|m| m.as_str().parse().unwrap_or(0_f64))
-                .unwrap_or(10000_f64);
+                .and_then(|file_stem| file_stem.to_str())
+            {
+                Some(file_stem) => file_stem.to_string(),
+                None => {
+                    return ExtensionResult::err("no filestem");
+                }
+            };
+            let number = match number_re.find(&file_name).ok().and_then(|m| m) {
+                Some(mat) => mat.as_str().parse().unwrap_or(0_f64),
+                None => 10000_f64,
+            };
 
             data.push(Chapter {
                 source_id: ID,
                 title: file_name,
-                path: entry.path().to_str().unwrap().to_string(),
+                path: format!("{}", entry.path().display()),
                 number,
                 scanlator: "".to_string(),
                 uploaded: NaiveDateTime::from_timestamp(modified as i64, 0),
@@ -177,7 +184,7 @@ impl Extension for Local {
             Ok(files) => {
                 let pages = files
                     .into_iter()
-                    .map(|p| path.clone().join(p).to_str().unwrap().to_string())
+                    .map(|p| format!("{}", path.clone().join(p).display()))
                     .collect();
                 ExtensionResult::ok(pages)
             }
