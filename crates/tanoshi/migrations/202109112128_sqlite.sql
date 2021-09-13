72e7d55272f589cc1a8fb6ce88556e47812efae9
faldez <github@faldez.dev>
2021-09-13 10:17:48 +0700

    change user_history migration

--- crates/tanoshi/migrations/202109112128_sqlite.sql
+++ crates/tanoshi/migrations/202109112128_sqlite.sql
@@ -1,4 +1,9 @@
 ALTER TABLE user_history ADD COLUMN is_complete BOOLEAN DEFAULT false;
 
+--- set chapter is complete if last page is the last page of the chapter
 UPDATE user_history
 SET is_complete = user_history.last_page =  (SELECT COUNT(1) - 1 FROM page WHERE page.chapter_id = user_history.chapter_id);
+
+--- set chapter is complete if last page is the second last page of the chapter
+UPDATE user_history
+SET is_complete = user_history.last_page =  (SELECT COUNT(1) - 2 FROM page WHERE page.chapter_id = user_history.chapter_id);
