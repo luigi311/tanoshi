6da7a9052a9b7956b8753d7c83ea97a0cb92e32a
faldez <github@faldez.dev>
2021-10-13 22:12:47 +0700

    replace three dots to filter icon

--- crates/tanoshi-web/src/manga.rs
+++ crates/tanoshi-web/src/manga.rs
@@ -650,7 +650,7 @@ impl Manga {
                                                     .attribute("stroke-linecap", "round")
                                                     .attribute("stroke-linejoin", "round")
                                                     .attribute("stroke-width", "2")
-                                                    .attribute("d", "M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z")
+                                                    .attribute("d", "M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z")
                                                 })
                                             ])
                                         }),
