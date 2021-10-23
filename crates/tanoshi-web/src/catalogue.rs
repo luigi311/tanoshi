5f56ac7e2f9b17e353eaf55d181fc039202cc426
faldez <github@faldez.dev>
2021-10-23 21:55:05 +0700

    replace text button with icon

--- crates/tanoshi-web/src/catalogue.rs
+++ crates/tanoshi-web/src/catalogue.rs
@@ -13,7 +13,7 @@ use crate::{
     common::{Cover, Spinner},
     utils::AsyncLoader,
 };
-use dominator::{clone, events, html, with_node, Dom};
+use dominator::{clone, events, html, svg, with_node, Dom};
 use futures_signals::map_ref;
 use futures_signals::signal::{Mutable, SignalExt};
 use futures_signals::signal_vec::{MutableVec, SignalVecExt};
@@ -150,7 +150,24 @@ impl Catalogue {
                     None
                 } else {
                     Some(html!("button", {
-                        .text("Filter")
+                        .attribute("id", "filter")
+                        .children(&mut [
+                            svg!("svg", {
+                                .attribute("xmlns", "http://www.w3.org/2000/svg")
+                                .attribute("fill", "none")
+                                .attribute("viewBox", "0 0 24 24")
+                                .attribute("stroke", "currentColor")
+                                .class("icon")
+                                .children(&mut [
+                                    svg!("path", {
+                                        .attribute("stroke-linecap", "round")
+                                        .attribute("stroke-linejoin", "round")
+                                        .attribute("stroke-width", "2")
+                                        .attribute("d", "M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z")
+                                    })
+                                ])
+                            }),
+                        ])
                     }))
                 }
             }))
@@ -184,7 +201,7 @@ impl Catalogue {
             .child_signal(catalogue.is_search.signal().map(clone!(catalogue => move |is_search| {
                 if is_search {
                     Some(html!("button", {
-                        .text("Cancel")
+                        .attribute("id", "cancel")
                         .event(clone!(catalogue => move |_: events::Click| {
                             catalogue.is_search.set_neq(false);
                             if catalogue.keyword.get_cloned() != "" {
@@ -194,13 +211,47 @@ impl Catalogue {
                                 Self::fetch_mangas(catalogue.clone());
                             }
                         }))
+                        .children(&mut [
+                            svg!("svg", {
+                                .attribute("xmlns", "http://www.w3.org/2000/svg")
+                                .attribute("fill", "none")
+                                .attribute("viewBox", "0 0 24 24")
+                                .attribute("stroke", "currentColor")
+                                .class("icon")
+                                .children(&mut [
+                                    svg!("path", {
+                                        .attribute("stroke-linecap", "round")
+                                        .attribute("stroke-linejoin", "round")
+                                        .attribute("stroke-width", "2")
+                                        .attribute("d", "M6 18L18 6M6 6l12 12")
+                                    })
+                                ])
+                            }),
+                        ])
                     }))
                 } else {
                     Some(html!("button", {
-                        .text("Search")
+                        .attribute("id", "search")
                         .event(clone!(catalogue => move |_: events::Click| {
                             catalogue.is_search.set_neq(true);
                         }))
+                        .children(&mut [
+                            svg!("svg", {
+                                .attribute("xmlns", "http://www.w3.org/2000/svg")
+                                .attribute("fill", "none")
+                                .attribute("viewBox", "0 0 24 24")
+                                .attribute("stroke", "currentColor")
+                                .class("icon")
+                                .children(&mut [
+                                    svg!("path", {
+                                        .attribute("stroke-linecap", "round")
+                                        .attribute("stroke-linejoin", "round")
+                                        .attribute("stroke-width", "2")
+                                        .attribute("d", "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z")
+                                    })
+                                ])
+                            }),
+                        ])
                     }))
                 }
             })))
--- crates/tanoshi-web/src/catalogue_list.rs
+++ crates/tanoshi-web/src/catalogue_list.rs
@@ -13,7 +13,7 @@ use crate::{
     common::{Cover, Spinner},
     utils::AsyncLoader,
 };
-use dominator::{clone, events, html, link, routing, with_node, Dom};
+use dominator::{clone, events, html, link, routing, svg, with_node, Dom};
 use futures_signals::signal::{Mutable, SignalExt};
 use futures_signals::signal_map::MutableBTreeMap;
 use futures_signals::signal_vec::{self, MutableVec, SignalVecExt};
@@ -135,7 +135,24 @@ impl CatalogueList {
                     None
                 } else {
                     Some(html!("button", {
-                        .text("Filter")
+                        .attribute("id", "filter")
+                        .children(&mut [
+                            svg!("svg", {
+                                .attribute("xmlns", "http://www.w3.org/2000/svg")
+                                .attribute("fill", "none")
+                                .attribute("viewBox", "0 0 24 24")
+                                .attribute("stroke", "currentColor")
+                                .class("icon")
+                                .children(&mut [
+                                    svg!("path", {
+                                        .attribute("stroke-linecap", "round")
+                                        .attribute("stroke-linejoin", "round")
+                                        .attribute("stroke-width", "2")
+                                        .attribute("d", "M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z")
+                                    })
+                                ])
+                            }),
+                        ])
                     }))
                 }
             }))
@@ -167,7 +184,7 @@ impl CatalogueList {
             .child_signal(catalogue.is_search.signal().map(clone!(catalogue => move |is_search| {
                 if is_search {
                     Some(html!("button", {
-                        .text("Cancel")
+                        .attribute("id", "cancel")
                         .event(clone!(catalogue => move |_: events::Click| {
                             catalogue.is_search.set_neq(false);
                             if catalogue.keyword.get_cloned() != "" {
@@ -177,13 +194,47 @@ impl CatalogueList {
                             local_storage().delete(STORAGE_KEY).unwrap_throw();
                             Self::fetch_sources(catalogue.clone());
                         }))
+                        .children(&mut [
+                            svg!("svg", {
+                                .attribute("xmlns", "http://www.w3.org/2000/svg")
+                                .attribute("fill", "none")
+                                .attribute("viewBox", "0 0 24 24")
+                                .attribute("stroke", "currentColor")
+                                .class("icon")
+                                .children(&mut [
+                                    svg!("path", {
+                                        .attribute("stroke-linecap", "round")
+                                        .attribute("stroke-linejoin", "round")
+                                        .attribute("stroke-width", "2")
+                                        .attribute("d", "M6 18L18 6M6 6l12 12")
+                                    })
+                                ])
+                            }),
+                        ])
                     }))
                 } else {
                     Some(html!("button", {
-                        .text("Search")
+                        .attribute("id", "search")
                         .event(clone!(catalogue => move |_: events::Click| {
                             catalogue.is_search.set_neq(true);
                         }))
+                        .children(&mut [
+                            svg!("svg", {
+                                .attribute("xmlns", "http://www.w3.org/2000/svg")
+                                .attribute("fill", "none")
+                                .attribute("viewBox", "0 0 24 24")
+                                .attribute("stroke", "currentColor")
+                                .class("icon")
+                                .children(&mut [
+                                    svg!("path", {
+                                        .attribute("stroke-linecap", "round")
+                                        .attribute("stroke-linejoin", "round")
+                                        .attribute("stroke-width", "2")
+                                        .attribute("d", "M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z")
+                                    })
+                                ])
+                            }),
+                        ])
                     }))
                 }
             })))
