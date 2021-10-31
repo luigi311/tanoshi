9ceaf1addf679988f1146f889e2556ba0e3f0486
faldez <github@faldez.dev>
2021-11-01 06:53:57 +0700

    download multiple chapter at once

--- crates/tanoshi/src/downloads.rs
+++ crates/tanoshi/src/downloads.rs
@@ -14,38 +14,49 @@ pub struct DownloadMutationRoot;
 
 #[Object]
 impl DownloadMutationRoot {
-    async fn download_chapter(&self, ctx: &Context<'_>, id: i64) -> Result<i64> {
+    async fn download_chapters(&self, ctx: &Context<'_>, ids: Vec<i64>) -> Result<i64> {
         if !user::check_is_admin(ctx)? {
             return Err("Forbidden".into());
         }
 
         let db = ctx.data::<MangaDatabase>()?;
-        let chapter = db.get_chapter_by_id(id).await?;
-        let manga = db.get_manga_by_id(chapter.manga_id).await?;
-        let pages = db.get_pages_by_chapter_id(id).await?;
-
         let ext = ctx.data::<ExtensionBus>()?;
-        let source = ext.detail(manga.source_id).await?;
 
-        let mut queue = vec![];
-        let date_added = Utc::now().naive_utc();
-        for (rank, page) in pages.iter().enumerate() {
-            queue.push(DownloadQueue {
-                id: 0,
-                source_name: source.name.clone(),
-                manga_title: manga.title.clone(),
-                chapter_title: chapter.title.clone(),
-                rank: rank as _,
-                url: page.clone(),
-                date_added,
-            })
+        let mut len = 0_usize;
+        for id in ids {
+            let chapter = db.get_chapter_by_id(id).await?;
+            let manga = db.get_manga_by_id(chapter.manga_id).await?;
+            let mut pages = db.get_pages_by_chapter_id(id).await?;
+
+            if pages.is_empty() {
+                pages = ext.get_pages(manga.source_id, chapter.path.clone()).await?;
+                db.insert_pages(chapter.id, &pages).await?;
+            }
+
+            let source = ext.detail(manga.source_id).await?;
+
+            let mut queue = vec![];
+            let date_added = Utc::now().naive_utc();
+            for (rank, page) in pages.iter().enumerate() {
+                queue.push(DownloadQueue {
+                    id: 0,
+                    source_name: source.name.clone(),
+                    manga_title: manga.title.clone(),
+                    chapter_title: chapter.title.clone(),
+                    rank: rank as _,
+                    url: page.clone(),
+                    date_added,
+                })
+            }
+            debug!("queue: {:?}", queue);
+            db.insert_download_queue(&queue).await?;
+            ctx.data::<Sender<WorkerCommand>>()?
+                .send(WorkerCommand::StartDownload)
+                .await?;
+
+            len += queue.len()
         }
-        debug!("queue: {:?}", queue);
-        db.insert_download_queue(&queue).await?;
-        ctx.data::<Sender<WorkerCommand>>()?
-            .send(WorkerCommand::StartDownload)
-            .await?;
 
-        Ok(queue.len() as _)
+        Ok(len as _)
     }
 }
