883a98ada1e691a83e12ab581c5d40b55e9eeab6
faldez <github@faldez.dev>
2021-11-12 20:52:52 +0700

    add lib.rs to use tanoshi as lib

---
+++ crates/tanoshi/src/lib.rs
@@ -0,0 +1,20 @@
+#[macro_use]
+extern crate log;
+extern crate argon2;
+
+#[cfg(feature = "embed")]
+pub mod assets;
+pub mod catalogue;
+pub mod config;
+pub mod db;
+pub mod downloads;
+pub mod library;
+pub mod local;
+pub mod notifier;
+pub mod proxy;
+pub mod schema;
+pub mod server;
+pub mod status;
+pub mod user;
+pub mod utils;
+pub mod worker;
