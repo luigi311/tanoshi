78b4a36976c6319fa1740033c1eb82897e260050
faldez <github@faldez.dev>
2021-11-12 20:53:46 +0700

    make proxy mor granulare and add get raw function

--- crates/tanoshi/src/proxy.rs
+++ crates/tanoshi/src/proxy.rs
@@ -45,13 +45,22 @@ impl Proxy {
 
     pub async fn get_image(&self, url: &str) -> Result<http::Response<Body>, StatusCode> {
         match url {
-            url if url.starts_with("http") => Ok(self.get_image_from_url(url).await?),
-            url if !url.is_empty() => Ok(self.get_image_from_file(url).await?),
+            url if url.starts_with("http") => Ok(self.get_image_from_url_stream(url).await?),
+            url if !url.is_empty() => {
+                let (content_type, data) = self
+                    .get_image_from_file(url)
+                    .await
+                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+                Ok(http::Response::builder()
+                    .header("Content-Type", content_type)
+                    .body(Body::from(data))
+                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?)
+            }
             _ => Err(StatusCode::BAD_REQUEST),
         }
     }
 
-    async fn get_image_from_file(&self, file: &str) -> Result<http::Response<Body>, StatusCode> {
+    async fn get_image_from_file(&self, file: &str) -> Result<(String, Vec<u8>), anyhow::Error> {
         let file = std::path::PathBuf::from(file);
 
         // if file is already a file, serve it
@@ -59,54 +68,44 @@ impl Proxy {
             let content_type = mime_guess::from_path(&file)
                 .first_or_octet_stream()
                 .to_string();
-            match tokio::fs::read(file).await {
-                Ok(buf) => Ok(http::Response::builder()
-                    .header("Content-Type", content_type)
-                    .body(Body::from(buf))
-                    .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?),
-                Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
-            }
+            let data = tokio::fs::read(file).await?;
+            Ok((content_type, data))
         } else {
             // else if its combination of archive files and path inside the archive
             // extract the file from archive
             let filename = file
                 .parent()
-                .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?
+                .ok_or(anyhow::anyhow!("no parent"))?
                 .display()
                 .to_string();
             let path = file
                 .file_name()
-                .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?
+                .ok_or(anyhow::anyhow!("no filename"))?
                 .to_str()
-                .ok_or(StatusCode::INTERNAL_SERVER_ERROR)?
+                .ok_or(anyhow::anyhow!("failed convert to string"))?
                 .to_string();
             let content_type = mime_guess::from_path(&path)
                 .first_or_octet_stream()
                 .to_string();
             let filename = filename.clone();
             let path = path.clone();
-            let buf: Vec<u8> =
-                tokio::task::spawn_blocking(move || -> Result<Vec<u8>, StatusCode> {
-                    let source = std::fs::File::open(filename)
-                        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-                    let mut buf: Vec<u8> = vec![];
-                    compress_tools::uncompress_archive_file(source, &mut buf, &path)
-                        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+            tokio::task::spawn_blocking(move || -> Result<(String, Vec<u8>), anyhow::Error> {
+                let source = std::fs::File::open(filename)?;
 
-                    Ok(buf)
-                })
-                .await
-                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)??;
+                let mut buf: Vec<u8> = vec![];
+                compress_tools::uncompress_archive_file(source, &mut buf, &path)?;
 
-            Ok(http::Response::builder()
-                .header("Content-Type", content_type)
-                .body(Body::from(buf))
-                .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?)
+                Ok((content_type, buf))
+            })
+            .await?
         }
     }
 
-    async fn get_image_from_url(&self, url: &str) -> Result<http::Response<Body>, StatusCode> {
+    async fn get_image_from_url_stream(
+        &self,
+        url: &str,
+    ) -> Result<http::Response<Body>, StatusCode> {
         debug!("get image from {}", url);
         if url.is_empty() {
             return Err(StatusCode::BAD_REQUEST);
@@ -129,5 +128,38 @@ impl Proxy {
         Ok(res
             .body(Body::wrap_stream(source_res.bytes_stream()))
             .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?)
+    }
+
+    #[allow(dead_code)]
+    async fn get_image_from_url(&self, url: &str) -> Result<(String, Vec<u8>), anyhow::Error> {
+        debug!("get image from {}", url);
+        if url.is_empty() {
+            return Err(anyhow::anyhow!("url is empty"));
+        }
+
+        let data = self.client.get(url).send().await?.bytes().await?.to_vec();
+
+        let content_type = mime_guess::from_path(url)
+            .first_or_octet_stream()
+            .to_string();
+
+        Ok((content_type, data))
+    }
+
+    #[allow(dead_code)]
+    pub async fn get_image_raw(&self, url: &str) -> Result<(String, Vec<u8>), anyhow::Error> {
+        let url = match utils::decrypt_url(&self.secret, url) {
+            Ok(url) => url,
+            Err(e) => {
+                error!("error validate url: {}", e);
+                "".to_string()
+            }
+        };
+
+        match url {
+            url if url.starts_with("http") => Ok(self.get_image_from_url(&url).await?),
+            url if !url.is_empty() => Ok(self.get_image_from_file(&url).await?),
+            _ => Err(anyhow::anyhow!("failed to get image")),
+        }
     }
 }
